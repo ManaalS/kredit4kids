3482a940999fda77900fb476b4a74cea1b4d79f2
Dana Feng <danafeng@berkeley.edu>
2021-10-23 15:43:21 -0700

    done

--- src/components/CreditScore.js
+++ src/components/CreditScore.js
@@ -1,64 +1,26 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Timer from "./Timer"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
-import { Button } from "@chakra-ui/react"
 import { CircularProgress, CircularProgressLabel, useDisclosure } from "@chakra-ui/react"
 import '.././index.css'
-import {
-  Slider,
-  SliderTrack,
-  SliderFilledTrack,
-  SliderThumb,
-} from "@chakra-ui/react"
-import { Box } from "@chakra-ui/react"
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from "@chakra-ui/react"
 
 
 
 function CreditScore(props) {
   const [creditScore, setCreditScore] = useState(props.creditScore);
-  const [moneyOwed, setMoneyOwed] = useState(props.moneyOwed)
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const [colorZone, setColorZone] = useState("green.400")
-  const [paymentAmount, setPaymentAmount] = useState(0)
-  const [minutes, setMinutes] = useState(props.minutes)
-  const [seconds, setSeconds] = useState(props.seconds)
 
-  function updateCreditScore() {
+  useEffect(() => {
     setCreditScore(creditScore)
     if (creditScore <= 589) {
       setColorZone("red.400")
     } else {
       setColorZone("green.400")
     }
-  }
-  
-  function resetTimer() {
-    setMinutes(0)
-    setSeconds(0)
-  }
+  }, [props.creditScore])
 
-  function submitPayment() {
-    setMoneyOwed(moneyOwed - paymentAmount)
-    updateCreditScore()
-    setPaymentAmount(0)
-    resetTimer()
-    onClose()
-  }
 
-  React.useEffect(() => {
-    // execute some code
-}, [moneyOwed])
   return (
     <Container className="credit-score">
       <Row>
@@ -67,39 +29,6 @@ function CreditScore(props) {
           <CircularProgress size="120px" value={creditScore / 10} color={colorZone}>
             <CircularProgressLabel>{creditScore}</CircularProgressLabel>
           </CircularProgress>
-        </Col>
-      </Row>
-      <Row>
-        <Col id="pay-card" >
-          <h1> Money You Need To Pay Back: ${moneyOwed} </h1>
-          <Button onClick={onOpen} colorScheme="green">
-            Pay Card
-          </Button>
-          <Modal onClose={onClose} isOpen={isOpen} isCentered>
-            <ModalOverlay/>
-            <ModalContent className="credit-score">
-              <ModalHeader>How much do you want to pay?</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <Slider onChange={(value) => { setPaymentAmount(value) }} defaultValue={0} min={0} max={moneyOwed} step={1}>
-                  <SliderTrack bg="blue.100">
-                    <Box position="relative" right={10} />
-                    <SliderFilledTrack bg="blue" />
-                  </SliderTrack>
-                  <SliderThumb boxSize={6} />
-                </Slider>
-                {paymentAmount}
-              </ModalBody>
-              <ModalFooter>
-                <Button onClick={() => submitPayment()}>Submit</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1> Next Payment Deadline: <Timer minutes={minutes} seconds={seconds}></Timer></h1>
         </Col>
       </Row>
     </Container>
--- src/components/Food.js
+++ src/components/Food.js
@@ -1,77 +1,169 @@
 import React, { useState, useEffect } from 'react';
+import Timer from "./Timer"
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Alert} from 'react-bootstrap';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 import CreditScore from './CreditScore';
 import { Button } from "@chakra-ui/react"
 import '.././index.css'
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react"
+import { useDisclosure } from "@chakra-ui/react"
 
 
 
 
 const Food = (props) => {
-  const [owed, setOwed] = useState(props.moneyOwed); 
+  const [minutes, setMinutes ] = useState(0);
+  const [seconds, setSeconds ] =  useState(0);
+  const [timerColor, setTimerColor] = useState("white")
+  const [owed, setOwed] = useState(props.moneyOwed);
   const [leftToBorrow, borrow] = useState(props.leftToBorrow);
   const [owned, updateMoney] = useState(props.moneyYouHave);
-  const [minutes, setMinutes] = useState(0)
-  const [seconds, setSeconds] = useState(0)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [paymentAmount, setPaymentAmount] = useState(0)
+
+  function resetTimer() {
+    setMinutes(0)
+    setSeconds(0)
+  }
+
+  function submitPayment() {
+    setOwed(owed - paymentAmount)
+    setPaymentAmount(0)
+    resetTimer()
+    onClose()
+  }
+
 
   function buyItem(price) {
-    if(owned >= price) {
+    if (owned >= price) {
       updateMoney(owned - price);
     }
     else {
       let remaining = owned - price;
-      setMinutes(5)
-      if(leftToBorrow >= -1*remaining) {
+      setMinutes(1)
+      if (leftToBorrow >= -1 * remaining) {
         borrow(leftToBorrow + remaining)
-        setOwed(-1*remaining + owed)
+        setOwed(-1 * remaining + owed)
       }
       else {
         console.log("in statement");
-        <Alert>You can't borrow any more!</Alert>
       }
     }
   }
 
+  function earnMoney() {
+    updateMoney(owned + 5)
+  }
+  useEffect(()=>{
+    let interval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            }
+            if (minutes == 0) {
+              setTimerColor("red")
+            } else {
+              setTimerColor("white")
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(interval)
+                } else {
+                    setMinutes(minutes - 1);
+                    setSeconds(59);
+                }
+            } 
+        }, 1000)
+        return () => {
+            clearInterval(interval);
+        };
+    });
   return (
     <div>
       <Container className="food">
         <Row>
           <Col>
-          <Row className="spacing">
-          <Col> 
-            <h1> Buy Food!</h1>
-            <p>Money you can borrow: ${leftToBorrow}</p>
-            <p>Money owed: {owed} </p> 
-           </Col>
-        </Row>
-        <Row className = "spacing">
-        <Button colorScheme="blue" onClick={() => buyItem(5)}><p>Buy an apple ($5)</p></Button> 
-        </Row>
-        <Row className = "spacing">
-        <Button colorScheme="blue" onClick={() => buyItem(10)}><p>Buy a fish ($10)</p></Button> 
-        </Row>
-        <Row className = "spacing">
-        <Button colorScheme="blue"  onClick={() => buyItem(15)}><p>Buy a mango ($15)</p></Button> 
-        </Row>
-          </Col>
-          <Col>
-          cat
-          </Col>
-          <Col>
-          <Col> 
-            <h1> Earn Money!</h1>
-            <p>Money you have: ${owned}</p>
-            <Row className = "spacing">
-            <Button colorScheme="blue"  onClick={() => buyItem(15)}><p>Perform a random act of kindness</p></Button> 
+            <Row className="spacing">
+              <Col>
+                <h1> Buy Food!</h1>
+                <p>Money you can borrow: ${leftToBorrow}</p>
+              </Col>
             </Row>
-           </Col>
+            <Row className="spacing">
+              <Button colorScheme="blue" onClick={() => buyItem(5)}><p>Buy an apple ($5)</p></Button>
+            </Row>
+            <Row className="spacing">
+              <Button colorScheme="blue" onClick={() => buyItem(10)}><p>Buy a fish ($10)</p></Button>
+            </Row>
+            <Row className="spacing">
+              <Button colorScheme="blue" onClick={() => buyItem(15)}><p>Buy a mango ($15)</p></Button>
+            </Row>
+          </Col>
+          <Col>
+            cat
+          </Col>
+          <Col>
+            <Col>
+              <h1> Earn Money!</h1>
+              <p>Money you have: ${owned}</p>
+              <Row className="spacing">
+                <Button colorScheme="blue" onClick={() => earnMoney(15)}><p>Perform a random act of kindness</p></Button>
+              </Row>
+            </Col>
           </Col>
         </Row>
-        
-        <CreditScore minutes ={minutes} seconds = {seconds} moneyOwed={owed} creditScore={750}></CreditScore>
+        <Row id="credit-score">
+          <CreditScore minutes={minutes} seconds={seconds} moneyOwed={owed} creditScore={750}></CreditScore>
+          <Col >
+            <h1> Money You Need To Pay Back: ${owed} </h1>
+            <Button onClick={onOpen} colorScheme="green">
+              Pay Card
+            </Button>
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
+              <ModalOverlay />
+              <ModalContent className="credit-score">
+                <ModalHeader>How much do you want to pay?</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Slider onChange={(value) => { setPaymentAmount(value) }} defaultValue={0} min={0} max={owed} step={1}>
+                    <SliderTrack bg="blue.100">
+                      <Box position="relative" right={10} />
+                      <SliderFilledTrack bg="blue" />
+                    </SliderTrack>
+                    <SliderThumb boxSize={6} />
+                  </Slider>
+                  {paymentAmount}
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={() => submitPayment()}>Submit</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Col>
+        </Row>
+        <Row>
+          Next payment deadline:
+            <Col>
+          Minutes: {minutes} Seconds: {seconds}
+        </Col>
+        </Row>
       </Container>
-      
+
     </div>
   );
 }
